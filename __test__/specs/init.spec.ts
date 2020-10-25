55fc379549a95013baf74810fe8972c550881983
Now11 <lightonw22@gmail.com>
2020-10-25 20:51:20 +0200

    add tests

---
+++ __test__/specs/init.spec.ts
@@ -0,0 +1,19 @@
+import { expect } from 'chai';
+
+describe('suite', () => {
+	it('test #1', () => {
+		expect(1).to.equals(1, 'Values does not match');
+	});
+
+	it('test #2', () => {
+		expect(1).to.equals(1, 'Values does not match');
+	});
+
+	it('test #3', () => {
+		expect(1).to.equals(1, 'Values does not match');
+	});
+
+	it('test #4', () => {
+		expect(1).to.equals(1, 'Values does not match');
+	});
+});
