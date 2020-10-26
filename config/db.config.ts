5117a720252423bae8b2a878a1dd7d121bdd0d2b
Now11 <lightonw22@gmail.com>
2020-10-26 16:15:55 +0200

    test

--- config/db.config.ts
+++ config/db.config.ts
@@ -2,18 +2,19 @@ import 'dotenv/config';
 import { Connection, createConnection } from 'typeorm';
 
 export const createPostgreSQLConnection = async (): Promise<Connection> => {
-	const connection = await createConnection({
-		type: process.env.TYPEORM_CONNECTION as any,
-		host: process.env.TYPEORM_HOST,
-		port: process.env.TYPEORM_PORT as any,
-		username: process.env.TYPEORM_USERNAME,
-		password: process.env.TYPEORM_PASSWORD,
-		database: process.env.TYPEORM_DATABASE,
-		logging: Boolean(process.env.TYPEORM_LOGGING),
-		entities: [process.env.TYPEORM_ENTITIES],
-		migrations: [process.env.TYPEORM_MIGRATIONS],
-		synchronize: false,
-	});
+	// const connection = await createConnection({
+	//     type: process.env.TYPEORM_CONNECTION as any,
+	//     host: process.env.TYPEORM_HOST,
+	//     port: process.env.TYPEORM_PORT as any,
+	//     username: process.env.TYPEORM_USERNAME,
+	//     password: process.env.TYPEORM_PASSWORD,
+	//     database: process.env.TYPEORM_DATABASE,
+	//     logging: Boolean(process.env.TYPEORM_LOGGING),
+	//     entities: [process.env.TYPEORM_ENTITIES],
+	//     migrations: [process.env.TYPEORM_MIGRATIONS],
+	//     synchronize: false,
+	// });
+	const c = await createConnection('postgres://postgres:postgres@pg:5432/test');
 
-	return connection;
+	return c;
 };
