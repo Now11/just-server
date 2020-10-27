fab4019e49ba2bf098c95bdb9e24bac6a11d36c7
Now11 <lightonw22@gmail.com>
2020-10-27 02:55:32 +0200

    test

--- config/db.config.ts
+++ config/db.config.ts
@@ -4,8 +4,7 @@ import { Connection, createConnection } from 'typeorm';
 export const createPostgreSQLConnection = async (): Promise<Connection> => {
 	const connection = await createConnection({
 		type: process.env.TYPEORM_CONNECTION as any,
-		// host: process.env.ENV === 'TEST' ? 'host.docker.internal' : process.env.TYPEORM_HOST,
-		host: 'host.docker.internal',
+		host: process.env.ENV === 'TEST' ? 'host.docker.internal' : process.env.TYPEORM_HOST,
 		port: process.env.TYPEORM_PORT as any,
 		username: process.env.TYPEORM_USERNAME,
 		password: process.env.TYPEORM_PASSWORD,
